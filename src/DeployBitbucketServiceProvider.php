6a2c655dbed1bc7099ac80860922d601b95239d2
BNHashem <a.hashem@thetailorsdev.com>
2021-11-13 12:54:23 +0200

    Adding Screenshots

---
+++ src/DeployBitbucketServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bnhashem\DeployBitbucket;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeployBitbucketServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__. '/bitbucket-pipelines.yml' => base_path('bitbucket-pipelines.yml'),
+            __DIR__. '/deployment-exclude-list.txt' => base_path('deployment-exclude-list.txt'),
+            __DIR__. '/Envoy.blade.php' => base_path('Envoy.blade.php'),
+        ]);
+        
+    }
+
+}
